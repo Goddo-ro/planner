d4b355e140fafa8430baa819f7c3729e49cf531b
Goddo-ro <r0manovic1@yandex.ru>
2023-09-30 20:58:43 +0300

    add form component

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -2,15 +2,18 @@ import ModalWindow from "./UI/ModalWindow/ModalWindow.jsx";
 import { closeEmail, isEmailShow } from "../store/modals.js";
 import { useStore } from "effector-react";
 import Button from "./UI/Button/Button.jsx";
+import Form from "./FormContainer/Form.jsx";
 
 const EmailForm = () => {
   const isOpen = useStore(isEmailShow)
 
   return(
     <ModalWindow isShow={isOpen} onClose={closeEmail}>
-      <h3>Вход</h3>
-      <input placeholder="E-mail"/>
-      <Button>Далее</Button>
+      <Form>
+        <h3 className="h2">Вход</h3>
+        <input placeholder="E-mail"/>
+        <Button>Далее</Button>
+      </Form>
     </ModalWindow>
   )
 }
--- src/store/modals.js
+++ src/store/modals.js
@@ -3,6 +3,6 @@ import { createEvent, createStore } from "effector";
 
 export const openEmail = createEvent();
 export const closeEmail = createEvent();
-export const isEmailShow = createStore(false)
+export const isEmailShow = createStore(true)
   .on(openEmail, () => true)
   .on(closeEmail, () => false);
