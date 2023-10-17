609d0b606823281dfdf23ad22ca5097e0ddc19c9
Goddo-ro <r0manovic1@yandex.ru>
2023-10-17 16:41:39 +0300

    rewrite modal using only dialog element

--- src/components/UI/ModalWindow/ModalWindow.jsx
+++ src/components/UI/ModalWindow/ModalWindow.jsx
@@ -1,18 +1,36 @@
-import "./ModalWindow.scss";
+import { useEffect, useRef } from "react";
 import {AiOutlineClose} from "react-icons/ai";
+import "./ModalWindow.scss";
 
 const ModalWindow = ({isShow = true, onClose, children, className}) => {
+  const dialogRef = useRef();
+
+  useEffect(() => {
+    if (dialogRef.current?.open && !isShow) {
+      dialogRef.current?.close();
+    } else if (!dialogRef.current?.open && isShow) {
+      dialogRef.current?.showModal();
+    }
+  }, [isShow]);
+
   const handleClick = (e) => {
-    if (e.target.className === "modal") onClose(e)
+    if (e.target.className === "modal") onClose(e);
   }
 
   return (
-    <div style={{display: `${isShow ? "flex" : "none"}`}} className="modal" onClick={handleClick}>
-      <dialog aria-modal="true" className={`modal__content ${className}`}>
-        <AiOutlineClose onClick={onClose} size={32} className="modal__close" fill="#B3B3BC" color="#B3B3BC" />
+    <dialog
+      aria-modal="true"
+      className="modal"
+      onClick={handleClick}
+      ref={dialogRef}
+    >
+      <div className={`modal__content ${className}`}>
+        <button tabIndex={0} disabled={false} role="button" className="modal__close" onClick={onClose}>
+          <AiOutlineClose size={32} fill="#B3B3BC" color="#B3B3BC" />
+        </button>
         {children}
-      </dialog>
-    </div>
+      </div>
+    </dialog>
   )
 }
 
